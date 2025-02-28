effe5ceea3fe259a2f467317e5acb74d47c9bc14
Alejandro Di Miscia <alejandro.di@enti.cat>
2025-02-28 09:46:43 +0100

    b

--- rollmania/src/PlayerName.js
+++ rollmania/src/PlayerName.js
@@ -3,9 +3,22 @@ import { useState } from 'react';
 
 function PlayerName(props) {
 	let [player_name, setPlayerName] = useState("");
+	let [show_name, setShowName] = useState(false);
+
+	function update_name (event) {
+		console.log(event.target.value);
+		setPlayerName(event.target.value);
+	}
+
+	function write_name () {
+		console.log("Nombre: "+player_name);
+	}
 
 	return (
-			<p><input type="text" name="player_name" placeholder="Nombre del jugador/a"/> <button>Guardar</button></p>
+			<p>
+			<input type="text" name="player_name" placeholder="Nombre del jugador/a" onChange={update_name} /> 
+			<button onClick={write_name}>Guardar</button>
+			</p>
   );
 }
 
